c186378c68c2eb266765f3d69497aa05b8d3d1a3
dzakki <dzakkiaz7@gmail.com>
2020-10-18 21:10:57 +0800

    quick-start 6-7

--- index.js
+++ index.js
@@ -87,18 +87,47 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening 133
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+
     let response;
 
-    // Check if the message contains text
+    // Checks if the message contains text
     if (received_message.text) {
-
-        // Create the payload for a basic text message
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
 
-    // Sends the response message
+    // Send the response message
     callSendAPI(sender_psid, response);
 }
 
